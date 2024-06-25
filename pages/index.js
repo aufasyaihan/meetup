9428354371ae6f44584aef343536660a3d996bcf
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-25 21:30:36 +0700

    refactor: Update MeetupList component to use destructuring and consistent import formatting

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://plus.unsplash.com/premium_photo-1714226830474-e0b8b733340b?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://images.unsplash.com/photo-1718809070510-371f29994edd?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    address: "Some address 10, 12345 Some City",
+    description: "This is a second meetup!",
+  },
+];
+
+export default function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
