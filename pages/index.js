0fe4ea363abcaed405a0105d63329f3e8e7919ca
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-25 22:08:50 +0700

    refactor: Update HomePage to use props for meetups

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,35 @@
 import MeetupList from "../components/meetups/MeetupList";
 import { DUMMY_MEETUPS } from "../lib/data";
 
-export default function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+export default function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
 }
+
+/**
+ * Fetches the static props for the index page.
+ * @returns {Promise<{props: {meetups: any[]}, revalidate: number}>} The static props object containing the meetups and revalidate value.
+ */
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 10, // revalidate every 10 seconds
+  };
+}
+
+/**
+ * Fetches data from the server and passes it as props to the component.
+ * This function is executed on the server side.
+ *
+ * @param {Object} context - The context object containing the request and response objects.
+ * @returns {Object} - An object containing the props to be passed to the component.
+ */
+// export async function getServerSideProps(context) {
+//   const { req, res } = context;
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
