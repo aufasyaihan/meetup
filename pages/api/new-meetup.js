48f5ec7b34aacac9fb6610083c5ed3cb6e3e86ff
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-25 22:35:16 +0700

    chore: Update npm dependency to mongodb@6.7.0

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb";
+
+export default async function postNewMeetupHandler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = MongoClient.connect(
+      "mongodb+srv://admin:excelsior%21@learn-nextjs.udjuyoj.mongodb.net/?retryWrites=true&w=majority&appName=learn-nextjs"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup added", meetup: result });
+  }
+}
